6e6e7d4d9f47aa81a8fa761ee2f1e4bcfa9abb14
J-M <jean-michel.perraud@csiro.au>
2018-10-25 14:03:24 +1100

    Increase unit testing cov for spatial operations

--- tests/test_spatial_dims.py
+++ tests/test_spatial_dims.py
@@ -12,6 +12,7 @@ sys.path.append(pkg_dir)
 from ela.textproc import *
 from ela.spatial import *
 from ela.classification import *
+from ela.io import GeotiffExporter
 
 def test_create_meshgrid():
     xx, yy = create_meshgrid_cartesian(x_min=0.0, x_max=1.1, y_min=1.0, y_max=1.51, grid_res = 0.5)
@@ -118,6 +119,7 @@ def test_slice_volume():
     dem[2,2] = np.nan
     dem[2,3] = -1.0
 
+    # TODO: I do not really like using drill_volume. Make sure this is unit tested itself.
     def f(x, y):
         return drill_volume(test_vol, dem, z_index_for_ahd, x, y)
 
@@ -134,6 +136,20 @@ def test_slice_volume():
     assert np.isnan(f(2,2))
     assert f(2,3) == test_vol[2,3,0]
 
+    s = slice_volume(test_vol, dem, z_index_for_ahd)
+    assert np.isnan(s[0,0])
+    assert np.isnan(s[0,1])
+    assert f(0,2) == s[0,2]
+    assert f(0,3) == s[0,3]
+    assert f(1,0) == s[1,0]
+    assert f(1,1) == s[1,1]
+    assert f(1,2) == s[1,2]
+    assert f(1,3) == s[1,3]
+    assert f(2,0) == s[2,0]
+    assert f(2,1) == s[2,1]
+    assert np.isnan(s[2,2])
+    assert f(2,3) == s[2,3]
+
 def get_test_bore_df():
     x_min = 383200
     y_max = 6422275
@@ -145,17 +161,28 @@ def get_test_bore_df():
         DEPTH_TO_COL: np.array(  [2.22, 3.33, 4.44, 5.55])
         })
 
-def test_raster_drill():
-    # crs = rasterio.crs.CRS({'proj': 'utm', 'zone': 50, 'south': True, 'ellps': 'GRS80', 'units': 'm', 'no_defs': True})
-    # # Upper left hand corner is at (x_min, y_max), and in raster terms this is the origin.
-    # x_min = 383200
-    # y_max = 6422275
-    # grid_res = 1
-    # from rasterio.transform import from_origin
-    # transform = from_origin(x_min, y_max, grid_res, grid_res)
-    # ge = GeotiffExporter(crs, transform)
+
+def create_test_slice(ni = 3, nj = 2, start=0.0, incr_1 = 1.0):
+    return np.array([
+        [(start + incr_1 * (i + ni * j)) for i in range(ni) ] for j in range(nj)
+    ])
+
+def get_slices_stack(n = 2, ni = 3, nj = 2, start=0.0, incr_1 = 1.0, incr_2 = 0.1):
+    x = create_test_slice(ni, nj, start, incr_1)
+    return [x + incr_2 * k for k in range(n)]
+
+def create_test_raster(x_min = 383200, y_max = 6422275, grid_res = 1 , ni = 2, nj = 2, start=1.0, incr_1 = 1.0, output_file='c:/tmp/test_raster_drill.tif'):
+    crs = rasterio.crs.CRS({'proj': 'utm', 'zone': 50, 'south': True, 'ellps': 'GRS80', 'units': 'm', 'no_defs': True})
+    # Upper left hand corner is at (x_min, y_max), and in raster terms this is the origin.
+    from rasterio.transform import from_origin
+    transform = from_origin(x_min, y_max, grid_res, grid_res)
+    ge = GeotiffExporter(crs, transform)
     # x = np.array([[1.0, 2.0],[3.0, 4.0]])
-    # ge.export_geotiff(x, 'c:/tmp/test_raster_drill.tif', None)
+    x = create_test_slice(ni, nj, start, incr_1)
+    ge.export_geotiff(x, output_file, None)
+
+def test_raster_drill():
+    # create_test_raster(x_min = 383200, y_max = 6422275, grid_res = 1 ,output_file='c:/tmp/test_raster_drill.tif'):
     x_min = 383200
     y_max = 6422275
     df = get_test_bore_df()
@@ -184,6 +211,37 @@ def test_add_ahd():
     assert to_ahd[2] == 2.0 - 4.44
     assert to_ahd[3] == 4.0 - 5.55
 
+
+def test_average_slices():
+    slices = get_slices_stack()
+    avg = average_slices(slices)
+    incr_2 = 0.1
+    assert avg[0,0] == incr_2 / 2
+    assert avg[0,1] == (incr_2 / 2) + 1.0
+    assert avg[1,0] == (incr_2 / 2) + 3.0
+
+
+
+# create_test_raster(x_min = 383200, y_max = 6422275, grid_res = 25 , ni = 10, nj = 10, start=0.0, incr_1 = 1.0, output_file=os.path.join(pkg_dir, 'tests', 'data', 'test_raster_25m.tif'))
+def test_surface_array():
+    dem = rasterio.open(os.path.join(pkg_dir, 'tests', 'data', 'test_raster_25m.tif'))
+    grid_res = 100
+    x_min = 383200 + 5 # first column
+    y_max = 6422275 - 5 # meaning falls within first row/band from top
+    y_min = y_max - (2 * grid_res) # 200 m offset over a 25m dem res, meaning falls within the 9th row/band from top
+    x_max = x_min + (2 * grid_res) # 200 m offset over a 25m dem res, meaning falls within the 9th column from left
+    surf_dem = surface_array(dem, x_min, y_min, x_max, y_max, grid_res)
+
+    assert surf_dem.shape[0] == 2
+    assert surf_dem.shape[1] == 2
+
+    assert surf_dem[0,0] == 80 + 0.0
+    assert surf_dem[0,1] == 80 + -4 * 10.0
+    assert surf_dem[1,0] == 80 + 4.0
+    assert surf_dem[1,1] == 80 + -4 * 10.0 + 4.0
+
+# test_surface_array()
+# test_average_slices()
 # test_slice_volume()
 # test_interpolate_slice()
 # test_burn_volume()
