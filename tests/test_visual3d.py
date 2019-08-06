e865088aed5c2536afed3190e4a2bd9669c62b90
J-M <jean-michel.perraud@csiro.au>
2019-08-06 18:09:55 +1000

    Try to include unit tests for mayavi - test how travis and codecov behave.

--- tests/test_visual3d.py
+++ tests/test_visual3d.py
@@ -27,21 +27,21 @@ from ela.visual3d import *
 
 
 # def do_not_test_yet_ui():
-# def test_mlab_ui():
-#     class_names = [
-#         'class_1',
-#         'class_2',
-#         'class_3',
-#         'class_4'
-#         ]
-#     color_names = ['red','orange','yellow','blue']
-#     vis = LithologiesClassesVisual3d(class_names, color_names, missing_value_color_name='black')
-#     assert vis.nb_labels() == len(class_names)
-#     assert vis.nb_labels_with_missing() == len(class_names) + 1
-#     volume = np.empty([2,3,4], dtype='float64')
-#     volume[:] = 0.0
-#     volume[:,:,1] = 1.0
-#     volume[:,:,2] = 2.0
-#     volume[:,:,3] = 3.0
-#     vis.render_classes_planar(volume,'blah title')
+def test_mlab_ui():
+    class_names = [
+        'class_1',
+        'class_2',
+        'class_3',
+        'class_4'
+        ]
+    color_names = ['red','orange','yellow','blue']
+    vis = LithologiesClassesVisual3d(class_names, color_names, missing_value_color_name='black')
+    assert vis.nb_labels() == len(class_names)
+    assert vis.nb_labels_with_missing() == len(class_names) + 1
+    volume = np.empty([2,3,4], dtype='float64')
+    volume[:] = 0.0
+    volume[:,:,1] = 1.0
+    volume[:,:,2] = 2.0
+    volume[:,:,3] = 3.0
+    vis.render_classes_planar(volume,'blah title')
 
