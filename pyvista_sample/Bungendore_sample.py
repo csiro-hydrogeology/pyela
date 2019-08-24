cab32a922f4749e58e346137bc4441acffda9b2f
J-M <jean-michel.perraud@csiro.au>
2019-08-24 17:23:12 +1000

    minor path to data adjusement

--- pyvista_sample/Bungendore_sample.py
+++ pyvista_sample/Bungendore_sample.py
@@ -1,12 +1,34 @@
 import pyvista as pv
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+pkg_dir = os.path.join(os.path.dirname(__file__),'..')
+
+sys.path.append(pkg_dir)
+
 from pyvista_sample.VisualizeDataProcess import VisualizeDataProcess
 
 '''
 A sample of 3D image based on pyvista
 
 '''
-drill_data_path = r"C:\Users\Dennis.H\Desktop\CSIRO_data\Bungendore\classified_logs.pkl"
-dem_data_path = r"C:\Users\Dennis.H\Desktop\CSIRO_data\Bungendore\dem_array_data.pkl"
+
+if ('ELA_DATA' in os.environ):
+    data_path = os.environ['ELA_DATA']
+elif sys.platform == 'win32':
+    data_path = r'C:\data\Lithology'
+else:
+    username = os.environ['USER']
+    data_path = os.path.join('/home', username, 'data', 'Lithology')
+
+drill_data_path = os.path.join(data_path, 'Bungendore','classified_logs.pkl')
+df = pd.read_pickle(drill_data_path)
+
+# drill_data_path = r"C:\Users\Dennis.H\Desktop\CSIRO_data\Bungendore\classified_logs.pkl"
+bungendore_datadir = os.path.join(data_path, 'Bungendore')
+dem_data_path = os.path.join(bungendore_datadir, 'dem_array_data.pkl')
 
 dp = VisualizeDataProcess()
 lines_dict = dp.drill_data_process(drill_data_path, 25)
