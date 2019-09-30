011ee43d0ed94819fba2db3fa45eb398975b4874
Dennis <7401444281@qq.com>
2019-09-30 22:31:46 +1000

    Generalized the data process

--- pyvista_sample/VisualizeDataProcess.py
+++ pyvista_sample/VisualizeDataProcess.py
@@ -354,6 +354,8 @@ class VisualizeDataProcess:
         threshed = volume.threshold([layer_from, layer_to])
         return threshed
 
+    #def exist_3d_lithology(self):
+
     def extract_single_lithology_class_3d(self, lithology_3d_classes, class_value):
         """Transform a 3D volume of lithology class codes by binary bining cells as being either of a class value or
         other. Preprocessing primarily for 3D visualisation for pyvista(not use in the sample ).
